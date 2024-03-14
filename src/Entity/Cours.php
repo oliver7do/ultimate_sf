35c2e47e8a8a5cc13898248d792d3846471f723e
oliver7do <afristreaming@gmail.com>
2024-03-14 16:54:33 +0100

    3 versao

---
+++ src/Entity/Cours.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CoursRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CoursRepository::class)]
+class Cours
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom_du_cours = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $prix = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $matiere = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomDuCours(): ?string
+    {
+        return $this->nom_du_cours;
+    }
+
+    public function setNomDuCours(string $nom_du_cours): static
+    {
+        $this->nom_du_cours = $nom_du_cours;
+
+        return $this;
+    }
+
+    public function getPrix(): ?string
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(string $prix): static
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getMatiere(): ?string
+    {
+        return $this->matiere;
+    }
+
+    public function setMatiere(string $matiere): static
+    {
+        $this->matiere = $matiere;
+
+        return $this;
+    }
+}
